f45c77cd43ea2668476dec2e52f0cbb6b7ae1351
Jung Seohyun <125573226+tjgus027@users.noreply.github.com>
2023-06-25 20:23:00 +0900

    Update 문자 리스트를 문자열로 변환하기.java
    내 코드 다시 추가, 다른 사람 풀이 참고 중 중요한 것 추가

--- 프로그래머스/unrated/181941. 문자 리스트를 문자열로 변환하기/문자 리스트를 문자열로 변환하기.java
+++ 프로그래머스/unrated/181941. 문자 리스트를 문자열로 변환하기/문자 리스트를 문자열로 변환하기.java
@@ -1,6 +1,14 @@
 class Solution {
     public String solution(String[] arr) {
+        //내가 쓴 정답
         String answer = "";
+        for(String s : arr){
+            answer += s;
+        }
+        return answer;
+        
+        /*다른 사람 풀이 참고
         return String.join("", arr);
+        */
     }
 }
