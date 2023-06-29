4b6678ddf2e6d1ea67ab7ae76a2fbe03314ade80
Jung Seohyun <125573226+tjgus027@users.noreply.github.com>
2023-06-29 20:40:27 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.02 ms, Memory: 70.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        int len = s.length()/2;
+        
+        if(s.length()%2 != 0){
+            answer = s.substring(len, len+1);
+        } else {
+            answer = s.substring(len-1, len+1);
+        }
+        return answer;
+    }
+}
