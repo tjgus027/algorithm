0fcec486a11b369a05d064af10cf264d11baf288
Jung Seohyun <125573226+tjgus027@users.noreply.github.com>
2023-07-01 15:45:51 +0900

    [unrated] Title: 문자열 바꿔서 찾기, Time: 0.16 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
+++ 프로그래머스/unrated/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
@@ -1,16 +1,21 @@
 class Solution {
     public int solution(String myString, String pat) {
-        String newStr = "";
+//         String newStr = "";
         
-        for(int i = 0; i < myString.length(); i++){
-            String str = String.valueOf(myString.charAt(i));
-            if(str.equals("A")){
-                newStr += "B";
-            } else {
-                newStr += "A";
-            }
-        }
+//         for(int i = 0; i < myString.length(); i++){
+//             String str = String.valueOf(myString.charAt(i));
+//             if(str.equals("A")){
+//                 newStr += "B";
+//             } else {
+//                 newStr += "A";
+//             }
+//         }
         
-        return (newStr.contains(pat)) ? 1 : 0;
+        //다른 사람 풀이 참고
+        myString = myString.replaceAll("A", "a");
+        myString = myString.replaceAll("B", "A");
+        myString = myString.replaceAll("a", "B");
+        
+        return (myString.contains(pat)) ? 1 : 0;
     }
 }
