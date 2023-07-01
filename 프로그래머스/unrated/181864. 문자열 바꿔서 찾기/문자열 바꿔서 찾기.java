16de683dd850bf3aa9d01fb5b1a44bd3e09d1133
Jung Seohyun <125573226+tjgus027@users.noreply.github.com>
2023-07-01 15:48:04 +0900

    Update 문자열 바꿔서 찾기.java
    문자열 바꿔서 찾기 중 주석 추가

--- 프로그래머스/unrated/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
+++ 프로그래머스/unrated/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
@@ -1,15 +1,19 @@
+/*
+확실히 밑에 다른 사람 풀이가 시간 복잡도 수치가 적어 효율적인 알고리즘임
+*/
 class Solution {
     public int solution(String myString, String pat) {
-//         String newStr = "";
+        //원래 내 풀이
+         String newStr = "";
         
-//         for(int i = 0; i < myString.length(); i++){
-//             String str = String.valueOf(myString.charAt(i));
-//             if(str.equals("A")){
-//                 newStr += "B";
-//             } else {
-//                 newStr += "A";
-//             }
-//         }
+        for(int i = 0; i < myString.length(); i++){
+            String str = String.valueOf(myString.charAt(i));
+            if(str.equals("A")){
+                newStr += "B";
+            } else {
+                newStr += "A";
+            }
+        }
         
         //다른 사람 풀이 참고
         myString = myString.replaceAll("A", "a");
