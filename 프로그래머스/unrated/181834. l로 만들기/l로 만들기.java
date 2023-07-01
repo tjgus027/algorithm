74de8459695e8471767d274b07e5354a15670886
Jung Seohyun <125573226+tjgus027@users.noreply.github.com>
2023-07-01 16:35:00 +0900

    Update l로 만들기.java
    주석 추가

--- 프로그래머스/unrated/181834. l로 만들기/l로 만들기.java
+++ 프로그래머스/unrated/181834. l로 만들기/l로 만들기.java
@@ -1,13 +1,18 @@
+/*
+원래 풀이가 시간복잡도, 공간복잡도 수치가 적었음
+*/
+
 class Solution {
     public String solution(String myString) {
-    //     //l = 108 - 아스키코드 참고
-    //     for(int i = 0; i < myString.length(); i++){
-    //         if(myString.charAt(i) < 108) {
-    //             myString = myString.replace(myString.charAt(i), 'l');
-    //         }
-    //     }
-    //     return myString;
-    // }
+        //원래 처음 풀이
+        //l = 108 - 아스키코드 참고
+        for(int i = 0; i < myString.length(); i++){
+            if(myString.charAt(i) < 108) {
+                myString = myString.replace(myString.charAt(i), 'l');
+            }
+        }
+        return myString;
+    }
         //다른 사람 풀이 참고 - 정규식표현 사용
         return myString.replaceAll("[^l-z]", "l");
     }
