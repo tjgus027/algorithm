1f3476e839f906e309d6e50f21348859395d9b61
Jung Seohyun <125573226+tjgus027@users.noreply.github.com>
2023-07-01 18:29:43 +0900

    [level 1] Title: 최소직사각형, Time: 1.13 ms, Memory: 89 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int[][] sizes) {
+        int max_col = 0;
+        int max_row = 0;
+        
+        for(int i = 0; i < sizes.length; i++){
+            if(sizes[i][0] < sizes[i][1]) {
+                int element = sizes[i][0];
+                sizes[i][0] = sizes[i][1];
+                sizes[i][1] = element;
+            }
+            if(max_row < sizes[i][0]) max_row = sizes[i][0];
+            if(max_col < sizes[i][1]) max_col = sizes[i][1];
+        }
+        
+        return max_col*max_row;
+    }
+}
