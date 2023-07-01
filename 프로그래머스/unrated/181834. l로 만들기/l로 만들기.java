fb0a3d7b93b1f568fb636a2fffcd64c77a43089f
Jung Seohyun <125573226+tjgus027@users.noreply.github.com>
2023-07-01 16:33:17 +0900

    [unrated] Title: l로 만들기, Time: 19.92 ms, Memory: 82.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181834. l로 만들기/l로 만들기.java
+++ 프로그래머스/unrated/181834. l로 만들기/l로 만들기.java
@@ -1,11 +1,14 @@
 class Solution {
     public String solution(String myString) {
-        //l = 108 - 아스키코드 참고
-        for(int i = 0; i < myString.length(); i++){
-            if(myString.charAt(i) < 108) {
-                myString = myString.replace(myString.charAt(i), 'l');
-            }
-        }
-        return myString;
+    //     //l = 108 - 아스키코드 참고
+    //     for(int i = 0; i < myString.length(); i++){
+    //         if(myString.charAt(i) < 108) {
+    //             myString = myString.replace(myString.charAt(i), 'l');
+    //         }
+    //     }
+    //     return myString;
+    // }
+        //다른 사람 풀이 참고 - 정규식표현 사용
+        return myString.replaceAll("[^l-z]", "l");
     }
 }
