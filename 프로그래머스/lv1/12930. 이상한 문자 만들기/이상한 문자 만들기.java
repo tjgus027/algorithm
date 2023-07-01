b7d5426c183081eff18af4b659e6521461228c4f
Jung Seohyun <125573226+tjgus027@users.noreply.github.com>
2023-07-01 18:02:13 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.44 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -1,22 +1,39 @@
 class Solution {
     public String solution(String s) {
-        String answer = "";
-        String[] str = s.split("");
+//         String answer = "";
+//         String[] str = s.split("");
         
-        int idx = 0; //인덱스
-        for(int i = 0; i < str.length; i++){
-            if(str[i].equals(" ")){
-                idx = 0; //공백이면 초기화
-            } else if (idx%2 == 0){
-                str[i] = str[i].toUpperCase();
-                idx++;
-            } else if (idx%2 != 0){
-                str[i] = str[i].toLowerCase();
-                idx++;
+//         int idx = 0;
+//         for(int i = 0; i < str.length; i++){
+//             //str[i] = str[i].toLowerCase();
+//             if(str[i].equals(" ")){
+//                 idx = 0;
+//             } else if (idx%2 == 0){
+//                 str[i] = str[i].toUpperCase();
+//                 idx++;
+//             } else if (idx%2 != 0){
+//                 str[i] = str[i].toLowerCase();
+//                 idx++;
+//             }
+//             answer += str[i];
+//         }
+        
+//         return answer;
+
+        //다른 사람 풀이 + sb 성능 테스트
+        StringBuilder sb = new StringBuilder();
+        int cnt = 0;
+        String[] array = s.split("");
+
+        for(String ss : array) {
+            cnt = ss.contains(" ") ? 0 : cnt + 1;
+            if(cnt%2 == 0){
+                sb.append(ss.toLowerCase());
+            } else {
+                sb.append(ss.toUpperCase());
             }
-            answer += str[i];
         }
         
-        return answer;
+        return sb.toString();
     }
 }
