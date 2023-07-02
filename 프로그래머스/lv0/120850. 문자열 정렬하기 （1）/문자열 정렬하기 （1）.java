5474db63ca2cf539242f9dbfc171437b2394a5a3
Jung Seohyun <125573226+tjgus027@users.noreply.github.com>
2023-07-02 17:33:35 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.49 ms, Memory: 71.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
+++ 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
@@ -2,18 +2,29 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String my_string) {
-        List<Character> list = new ArrayList<>();
-        for(int i = 0; i < my_string.length(); i++){
-            if(my_string.charAt(i) > 47 && my_string.charAt(i) < 58){
-                list.add(my_string.charAt(i));
-            }
-        }
+//         List<Character> list = new ArrayList<>();
+//         for(int i = 0; i < my_string.length(); i++){
+//             if(my_string.charAt(i) > 47 && my_string.charAt(i) < 58){
+//                 list.add(my_string.charAt(i));
+//             }
+//         }
         
-        int[] answer = new int[list.size()];
-        for(int i = 0; i < list.size(); i++){
-            answer[i] = list.get(i) - 48;
-        }
+//         int[] answer = new int[list.size()];
+//         for(int i = 0; i < list.size(); i++){
+//             answer[i] = list.get(i) - 48;
+//         }
         
+//         Arrays.sort(answer);
+        
+//         return answer;
+        
+        //다른 사람 풀이 참고해서 수정
+        my_string = my_string.replaceAll("[^0-9]", "");
+        
+        int[] answer = new int[my_string.length()];
+        for(int i = 0; i < answer.length; i++){
+            answer[i] = my_string.charAt(i) - 48;
+        }
         Arrays.sort(answer);
         
         return answer;
