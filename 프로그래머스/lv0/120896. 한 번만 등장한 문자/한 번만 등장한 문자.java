dc1bc370c464dfd5e854a586dedfbb298386c9b4
Jung Seohyun <125573226+tjgus027@users.noreply.github.com>
2023-07-06 17:51:21 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 2.41 ms, Memory: 72.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        String[] arr = s.split("");
+        Arrays.sort(arr);
+        
+        for(int i = 0; i < arr.length; i++){
+            int cnt = 0;
+            for(int j = 0; j < arr.length; j++){
+                if(arr[i].equals(arr[j])) cnt++;
+            }
+            if(cnt == 1){
+                answer += arr[i];
+            }
+            
+        }
+        return answer;
+    }
+}
