da9785a49a73254b116a672daac9c1b47ee2a66b
Jung Seohyun <125573226+tjgus027@users.noreply.github.com>
2023-07-07 19:07:13 +0900

    [unrated] Title: 리스트 자르기, Time: 1.91 ms, Memory: 74.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181897. 리스트 자르기/리스트 자르기.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n, int[] slicer, int[] num_list) {
+        List<Integer> list = new ArrayList<>();
+        switch(n) {
+            case 1: 
+            for(int i = 0; i <= slicer[1]; i++){
+                list.add(num_list[i]);
+            }
+            break;
+            
+            case 2:
+            for(int i = slicer[0]; i < num_list.length; i++){
+                list.add(num_list[i]);
+            }
+            break;
+            
+            case 3:
+            for(int i = slicer[0]; i <= slicer[1]; i++){
+                list.add(num_list[i]);
+            }
+            break;
+            
+            case 4:
+            for(int i = slicer[0]; i <= slicer[1]; i = i+slicer[2]){
+                list.add(num_list[i]);
+            }
+            break;
+            
+        }
+        return list.stream().mapToInt(i -> i.intValue()).toArray();
+    }
+}
