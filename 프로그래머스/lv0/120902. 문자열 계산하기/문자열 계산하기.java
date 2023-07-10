155bf239940b2f2dd14cb3754ac1cd903844e6d4
Jung Seohyun <125573226+tjgus027@users.noreply.github.com>
2023-07-10 20:43:10 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.10 ms, Memory: 75.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120902. 문자열 계산하기/문자열 계산하기.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String my_string) {
+        String[] arr = my_string.split(" ");
+        int answer = Integer.parseInt(arr[0]);
+        
+        for(int i = 0; i < arr.length; i++){
+            if(i%2 != 0){
+                if(arr[i].equals("+")){
+                    answer += Integer.parseInt(arr[i+1]);
+                } else {
+                    answer -= Integer.parseInt(arr[i+1]);
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
