70ac46b0a38691816281e1a4603fc56cdae50370
Jung Seohyun <125573226+tjgus027@users.noreply.github.com>
2023-07-23 21:43:54 +0900

    [level 2] Title: 주식가격, Time: 25.57 ms, Memory: 68.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] prices) {
+        Stack<Integer> stack = new Stack<>();
+        
+        for(int i = 0 ; i < prices.length; i++){
+            stack.push(prices.length-1-i);
+            for(int j = i; j < prices.length; j++){
+                if(prices[i] > prices[j]) {
+                    stack.pop();
+                    stack.push(j - i);
+                    break;
+                }
+            }
+        }
+        return stack.stream().mapToInt(i -> i.intValue()).toArray();
+    }
+}
