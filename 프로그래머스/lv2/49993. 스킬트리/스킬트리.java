2effdf83f8486a5387caa76ee1c4cb95aa8f1f9d
Jung Seohyun <125573226+tjgus027@users.noreply.github.com>
2023-08-03 22:59:05 +0900

    [level 2] Title: 스킬트리, Time: 9.60 ms, Memory: 82.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/49993. 스킬트리/스킬트리.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(String skill, String[] skill_trees) {
+        int answer = 0;
+        
+        for(int i = 0; i < skill_trees.length; i++){
+            skill_trees[i] = skill_trees[i].replaceAll("[^" + skill + "]", "");
+            if(skill.indexOf(skill_trees[i]) == 0) answer++;
+        }
+        
+        return answer;
+    }
+}
