b90a998a325958a07d37c79ab80ab4f935177309
Jung Seohyun <125573226+tjgus027@users.noreply.github.com>
2023-08-12 16:11:02 +0900

    [level 2] Title: 카펫, Time: 0.02 ms, Memory: 77.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42842. 카펫/카펫.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int brown, int yellow) {
+        int[] answer = {0,0};
+        int total = brown + yellow;
+        for(int i = 3; i <= total; i++){
+            int row = i;
+            int col = total / row;
+            
+            if(row > col) continue;
+            if((row-2) * (col-2) == yellow){
+                answer[0] = col;
+                answer[1] = row;
+                return answer;
+            }
+        }
+        return answer;
+    }
+}
