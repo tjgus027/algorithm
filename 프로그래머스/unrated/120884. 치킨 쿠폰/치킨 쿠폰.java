c12eb7cc0547e9c322f91261329cdfc1d162a4f9
Jung Seohyun <tjgus027@kakao.com>
2023-10-14 19:07:40 +0900

    [level unrated] Title: 치킨 쿠폰, Time: 0.02 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/unrated/120884. 치킨 쿠폰/치킨 쿠폰.java
+++ 프로그래머스/unrated/120884. 치킨 쿠폰/치킨 쿠폰.java
@@ -1,15 +1,9 @@
 class Solution {
-    private final int BONUS_NUM = 10;
-    
     public int solution(int chicken) {
         int answer = 0;
-        
-        while(chicken >= BONUS_NUM){
-            int newChicken = chicken/BONUS_NUM;
-            int restChi = chicken%BONUS_NUM;
-            
-            chicken = newChicken + restChi;
-            answer += newChicken;
+        while(chicken >= 10){
+            answer += chicken/10;
+            chicken = (chicken/10) + (chicken%10);
         }
         return answer;
     }
