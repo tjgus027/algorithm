1568286b96a9c65b3e88ba81745f154ce2043aa1
Jung Seohyun <tjgus027@kakao.com>
2023-12-11 16:30:36 +0900

    [level 3] Title: 정수 삼각형, Time: 13.48 ms, Memory: 60 MB -BaekjoonHub

---
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -0,0 +1,21 @@
+import java.math.*;
+
+class Solution {
+    public int solution(int[][] triangle) {
+        int answer = 0;
+        
+        for(int i = 1; i < triangle.length; i ++){
+            for(int j = 0; j < triangle[i].length; j++){
+                if(j == 0){
+                    triangle[i][j] += triangle[i-1][j];
+                } else if(j == i){
+                    triangle[i][j] += triangle[i-1][j-1];
+                } else {
+                    triangle[i][j] += Math.max(triangle[i-1][j], triangle[i-1][j-1]);
+                }
+                answer = Math.max(answer, triangle[i][j]);
+            }
+        }
+        return answer;
+    }
+}
