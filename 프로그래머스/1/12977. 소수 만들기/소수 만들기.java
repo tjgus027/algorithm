fd22fe7785acf0ccde9e0b07f393a24f883728f1
Jung Seohyun <tjgus027@kakao.com>
2023-12-15 23:36:53 +0900

    [level 1] Title: 소수 만들기, Time: 0.02 ms, Memory: 73.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -0,0 +1,25 @@
+import java.math.*;
+
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+        
+        for (int i = 0; i < nums.length; i++){
+			for (int j = i + 1; j < nums.length; j++){
+				for (int k = j + 1; k < nums.length; k++){
+					int sum = nums[i] + nums[j] + nums[k];
+					answer += isPrime(sum) ? 1 : 0;
+				}
+			}
+		}
+        
+        return answer;
+    }
+    
+    private boolean isPrime(int num){
+        for(int i = 2; i <= Math.sqrt(num); i++){
+            if(num%i == 0) return false;
+        }
+        return true;
+    }
+}
