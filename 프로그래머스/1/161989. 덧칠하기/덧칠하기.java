77a3d79c49e1a10cc263a8028e7346be81991bde
Jung Seohyun <tjgus027@kakao.com>
2023-12-20 17:40:52 +0900

    [level 1] Title: 덧칠하기, Time: 1.39 ms, Memory: 78.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n, int m, int[] section) {
+        int answer = 0;
+        int temp = 0;
+        for(int i = 0; i < section.length; i++){
+            if(section[i] < temp) continue;
+            answer++;
+            temp = section[i] + m;
+        }
+        return answer;
+    }
+}
