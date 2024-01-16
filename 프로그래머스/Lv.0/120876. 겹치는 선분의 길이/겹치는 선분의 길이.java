ec5b247098d9a6b827a55aa65f959dd3ed1f8d21
Jung Seohyun <tjgus027@kakao.com>
2024-01-16 20:22:58 +0900

    [level Lv.0] Title: 겹치는 선분의 길이, Time: 0.03 ms, Memory: 75.9 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] lines) {
+        int answer = 0;
+        
+        int[] count = new int[201];
+        
+        for(int[] line : lines){
+            int start = line[0] + 100;
+            int end = line[1] + 100;
+            
+            for(int k = start; k < end; k++){
+                count[k] += 1;
+            }
+        }
+        
+        for(int i : count){
+            if(i > 1) answer++;
+        }
+        
+        return answer;
+    }
+}
