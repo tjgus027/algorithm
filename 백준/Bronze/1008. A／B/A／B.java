e318bc6c9a319838104ed54b227761e997b0de99
Jung Seohyun <tjgus027@kakao.com>
2024-01-26 21:01:09 +0900

    [Bronze V] Title: A/B, Time: 204 ms, Memory: 17788 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        double A = sc.nextInt();
+        double B = sc.nextInt();
+        
+        System.out.println(A/B);
+    }
+}
