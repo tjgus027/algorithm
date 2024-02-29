2f0b1c0a81325c6c016ea72430a95b44a0023127
Jung Seohyun <tjgus027@kakao.com>
2024-02-29 23:01:13 +0900

    [level 1] Title: 달리기 경주, Time: 0.03 ms, Memory: 75.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[] players, String[] callings) {
+        Map<String, Integer> rankMap = new HashMap<>();
+        for(int i = 0; i < players.length; i++){
+            rankMap.put(players[i], i);
+        }
+        
+        for(String player : callings){
+            int ownRank = rankMap.get(player);
+            String beforePlayer = players[ownRank-1];
+            
+            players[ownRank-1] = player;
+            players[ownRank] = beforePlayer;
+            
+            rankMap.put(player, ownRank-1);
+            rankMap.put(beforePlayer, ownRank);
+        }
+        
+        return players;
+    }
+}
